e9ee01aa8225d33bbf9c40a7c4b598cfcad57221
yi-jou-huang <df467289@gmail.com>
2024-08-31 10:09:36 +0800

    first commit

---
+++ calculator.js
@@ -0,0 +1,21 @@
+﻿let input = document.getElementById("cal");
+let appendIt = (e)=>{
+    input.value += e;
+}
+let clearNum = ()=>{
+    input.value = "";
+}
+let calculate = ()=>{
+    try{
+        input.value  = eval(input.value);
+    }catch(error){
+        input.value = 'Error';
+        clearNum()
+    }
+}
+
+/* 
+eval("string"+"string") 
+    allows you to execute a string of code as if it were actual JavaScript code.
+try{} catch(err){}
+*/
